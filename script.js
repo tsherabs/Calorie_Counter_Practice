654b2702ffad19fd37637a98655d742ba2647d48
tsherabs <jdamchoe@gmail.com>
2026-07-06 00:07:32 +0600

    added scriptfile for functionalities

---
+++ script.js
@@ -0,0 +1,8 @@
+const calorieCounter = document.getElementById('calorie-counter')
+const calorieInput = document.getElementById('calorie-input'); 
+const addFoodEx = document.getElementById('add-food-exercise'); 
+const addEntry = document.getElementById('add-entry'); 
+const clearBtn = document.getElementById('clear'); 
+let isError = false; 
+
+
