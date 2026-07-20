c5656a1ee8dcbab7c5486a4003270f30e7f1c455
tsherabs <jdamchoe@gmail.com>
2026-07-20 19:44:31 +0600

    added the regex expression to detect + or - signs in the input, also starting off with hwo to use value property.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const calorieCounter = document.getElementById('calorie-counter')
 const calorieInput = document.getElementById('calorie-input'); 
-const addFoodEx = document.getElementById('add-food-exercise'); 
+const entryDropdown = document.getElementById('entryDropdown'); 
 const addEntry = document.getElementById('add-entry'); 
 const clearBtn = document.getElementById('clear'); 
 let isError = false; 
 
+function checkValid(str){
+    const regex = /[+-\s]/g; 
+    return str.replace(regex, ""); 
+}
 
+function isInvalidCheck(str){
+    const regex = /\d+e\d+/; 
+    return str.match(regex); 
+}
+
+function addEntry(){
+    const targetId = "#" + entryDropdown.value; 
+    
+}
